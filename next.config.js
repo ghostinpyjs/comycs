bcd8fba55e57f4a6c3d11b3771bfc10deccec2a3
ghostinpyjs <bernardorodrigues161118@gmail.com>
2026-05-25 07:37:09 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.steamstatic.com', 'community.cloudflare.steamstatic.com', 'steamcdn-a.akamaihd.net', 'community.akamai.steamstatic.com'],
+    domains: [
+      'avatars.steamstatic.com',
+      'community.cloudflare.steamstatic.com',
+      'steamcdn-a.akamaihd.net',
+      'community.akamai.steamstatic.com'
+    ],
   },
 }
-
 module.exports = nextConfig
